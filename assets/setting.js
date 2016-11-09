eec7a5d3ef6063c3beed026ac1dcfc330cce222a
nguyenchihien <hiennc@onlikeservices.com>
2016-11-09 17:05:03 +0700

    demo

--- assets/setting.js
+++ assets/setting.js
@@ -37,7 +37,7 @@ var colors = [
     // "brown", "grey", "blue-grey"      // 16 17 18 19
 ];
 
-// colors = ['green', 'deep-purple']
+colors = ['green']
 var i = Math.floor( Math.random() * (colors.length - 0) + 0 );
 var chooseColor = colors[i].toString();
 
